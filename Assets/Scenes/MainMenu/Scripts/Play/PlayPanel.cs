24e9c352e0480747d2e9e5dfac647eac81e54cb2
Alex <130986014+AylexCODE@users.noreply.github.com>
2026-04-12 23:59:38 +0800

    -Hotpush, configuring play mode

--- Assets/Scenes/MainMenu/Scripts/Play/PlayPanel.cs
+++ Assets/Scenes/MainMenu/Scripts/Play/PlayPanel.cs
@@ -1,13 +1,42 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayPanel : MonoBehaviour {
     [SerializeField] private GameObject panel;
+    [SerializeField] private float fadeDuration = 1f;
 
-    void Start() {
+    private CanvasGroup canvas;
+    private Coroutine activeAnimation;
+
+    void Start () {
+        canvas = GetComponent<CanvasGroup>();
         panel.SetActive(false);
+        canvas.alpha = 0f;
     }
 
     public void OpenPlayPanel() {
         panel.SetActive(true);
+
+        if(activeAnimation != null) StopCoroutine(activeAnimation);
+        activeAnimation = StartCoroutine(Fade(true, 1f));
+    }
+
+    public void ClosePlayPanel() {
+        if(activeAnimation != null) StopCoroutine(activeAnimation);
+        activeAnimation = StartCoroutine(Fade(false, 0f));
+    }
+
+    private IEnumerator Fade (bool setActive, float alpha) {
+        float startAlpha = canvas.alpha;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration) {
+            elapsedTime += Time.deltaTime;
+            canvas.alpha = Mathf.Lerp(startAlpha, alpha, elapsedTime / fadeDuration);
+            yield return null;
+        }
+
+        if(!setActive) panel.SetActive(false);
+        canvas.alpha = alpha;
     }
 }
